246f346b2a5f64c8cf203cdf58a0501512ed1976
Laurat <l4tt1haga@gmail.com>
2023-08-28 23:09:31 +0200

    adding header component

---
+++ client/src/components/header/Header.tsx
@@ -0,0 +1,13 @@
+import './style.css';
+
+export default function Header() {
+    return (
+        <header>
+            <nav>
+                <a href="#">Home</a>
+                <a href="#">About</a>
+                <a href="#">Contact</a>
+            </nav>
+        </header>
+    );
+}
